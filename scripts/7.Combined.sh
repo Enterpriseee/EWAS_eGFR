f9b2e5a754b6ed62b524c46a41e1ff6c56739c11
Qi <132986202+Enterpriseee@users.noreply.github.com>
2024-12-23 16:10:47 +0800

    Add files via upload

---
+++ scripts/7.Combined.sh
@@ -0,0 +1,7 @@
+conda activate
+conda env list
+conda deactivate
+conda activate combined-pvalues-2.7
+cd /home/DNAm_eGFR
+bash /home/DNAm_eGFR/scripts/7_combined-pvalues.sh
+cat scripts/7_combined-pvalues.sh
